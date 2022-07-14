19b29936eee3a114d549c8ebd99b162442b023bf
Kauã Muniz <kauamunizz@hotmail.com>
2022-07-14 01:39:35 -0300

    projeto 11

---
+++ 11.Montador de endereco/scripts.js
@@ -0,0 +1,13 @@
+let endereco = {
+    rua: 'a',
+    cidade: 'b',
+    cep: 'c'
+}
+
+function exibirEndereco(endereco) {
+    for(let key in endereco){
+        console.log(key,endereco[key]);
+    }
+}
+
+exibirEndereco(endereco);
