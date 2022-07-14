371c96611af65c2ce3dc714cee48cf20da97dac7
Kauã Muniz <kauamunizz@hotmail.com>
2022-07-14 01:39:47 -0300

    projeto 12

---
+++ 12.Encontre a Igualdade/scripts.js
@@ -0,0 +1,21 @@
+function Endereco(rua, cidade, cep) {
+    this.rua = rua,
+    this.cidade = cidade,
+    this.cep = cep
+}
+
+const endereco1 = new Endereco('a','b','c');
+const endereco2 = new Endereco('a','b','c');
+
+function saoIguais(endereco1,endereco2) {
+    return endereco1.rua === endereco2.rua &&
+    endereco1.cidade === endereco2.cidade &&
+    endereco1.cep === endereco2.cep   
+}
+
+function enderecoMemoriaIgual(endereco1, endereco2) {
+    return endereco1 === endereco2;
+}
+
+console.log(saoIguais(endereco1, endereco2))
+console.log(enderecoMemoriaIgual(endereco1, endereco2))
