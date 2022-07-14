1c558d5755dbfa6608a590357e556626ea57dea5
Kauã Muniz <kauamunizz@hotmail.com>
2022-07-14 01:38:52 -0300

    projeto 7

---
+++ 7.Multiplos de 3 e 5/scripts.js
@@ -0,0 +1,15 @@
+function somar(limite) {
+    let multiplosDe3 = 0;
+    let multiplosDe5 = 0;
+    for(let x = 0; x <= limite; x++){
+        if (x %3 == 0) {
+            multiplosDe3 += x;
+        } else if  (x % 5 == 0) {
+            multiplosDe5 += x;
+        }
+    }
+    console.log(multiplosDe3 + multiplosDe5);
+}
+
+
+somar(10)
