ec8b3f9aaf269d7433838272790479da185780ac
Kauã Muniz <kauamunizz@hotmail.com>
2022-07-14 01:36:49 -0300

    projeto 2

---
+++ 2.Maximo entre dois valores/scripts.js
@@ -0,0 +1,10 @@
+let valorMaior = max(52,97);
+console.log(valorMaior);
+
+function max(numero1, numero2) {
+    if (numero1 > numero2) {
+        return numero1;
+    } else {
+        return numero2;
+    }
+}
